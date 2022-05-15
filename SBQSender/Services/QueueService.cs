f67ee39a4baa5e456b22c62256acbad53b91c808
sneha095 <kuriansneha95@gmail.com>
2022-05-15 11:27:59 +0530

    Added : Queservice to sendmessage

---
+++ SBQSender/Services/QueueService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SBQSender.Services
+{
+    public class QueueService
+    {
+        private readonly IConfiguration _config;
+        public QueueService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendMessage<T>(T serviceBusMessage , string queueName)
+        {
+            var queueClient = new QueueClient(_config.GetConnectionString("AzureServiceBus"), queueName);
+            var messageBody = JsonSerializer.Serialize(serviceBusMessage);
+            var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+            await queueClient.SendAsync(message);
+        }
+    }
+}
